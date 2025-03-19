45ba73b6f2efe64fde225ea048450b19375591aa
Gabriel Pontes <105681324+GabrielPontesBT@users.noreply.github.com>
2025-03-19 11:40:33 -0300

    Cambios esteticos (#1)
    Co-authored-by: GabrielPontes04 <105681324+GabrielPontes04@users.noreply.github.com>

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="bg-white text-dark">
+      <q-toolbar class="bg-white text-dark" style="height: 80px;">
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          BT-Servicess
+          Inventario de Tablas
         </q-toolbar-title>
 
         <div>BT v{{ $q.version }}</div>
@@ -30,7 +30,7 @@
             </q-menu>
           </q-btn> -->
           <q-btn round flat>
-            <q-avatar size="26px">
+            <q-avatar size="30px">
               <img src="https://cdn.quasar.dev/img/boy-avatar.png">
             </q-avatar>
             <q-menu>
@@ -48,126 +48,48 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-dark text-white">
+      <q-img src="~assets/logo.png" width="280px" />
       <q-list>
+        <!--
         <q-item-label header class="text-white">
           <q-btn round dense flat style="color:red !important;">
             <i class="fa fa-heart fa-1.5x fa-beat"></i>
           </q-btn>
-          By Bantotal 
         </q-item-label>
+        -->
         <q-item to="/" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
             <q-icon name="dashboard" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Dashboard</q-item-label>
+            <q-item-label>Inicio</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item to="/sistemas" active-class="q-item-no-link-highlighting">
-          <q-item-section avatar>
-            <q-icon name="card_giftcard" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Sistemas</q-item-label>
-          </q-item-section>
-        </q-item>
-        <!-- <q-expansion-item icon="pages" label="Ahorro Programado">
-          <q-list class="q-pl-lg">
-            <q-item to="/Login-1" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="email" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Login-1</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Lock" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="lock" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Lock Screen</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Lock-2" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="lock" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Lock Screen - 2</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Pricing" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Pricing</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item-label header class="text-weight-bolder text-white">Generic</q-item-label>
-            <q-item to="/Profile" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="person" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>User Profile</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/Productos" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="settings" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Productos</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-expansion-item>
-        <q-expansion-item icon="map" label="CadenaCierre">
-          <q-list class="q-pl-lg">
-            <q-item to="/Map" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="map" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Map</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/MapMarker" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="location_on" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Map Marker</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item to="/StreetView" active-class="q-item-no-link-highlighting">
-              <q-item-section avatar>
-                <q-icon name="streetview" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Street View</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-expansion-item> -->
 
         <q-item to="/areas" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
-            <q-icon name="email" />
+            <q-icon name="group" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Areas Funcionales</q-item-label>
+            <q-item-label>Áreas Funcionales</q-item-label>
           </q-item-section>
         </q-item>
 
         <q-item to="/paises" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
-            <q-icon name="card_giftcard" />
+            <q-icon name="fas fa-globe-americas" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Paìses</q-item-label>
+            <q-item-label>Países</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item to="/sistemas" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Sistemas</q-item-label>
           </q-item-section>
         </q-item>
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,19 @@
 <template>
   <q-page class="q-pa-md">
-    <!-- Botón para crear nuevo registro -->
-    <q-btn label="Crear Nuevo" color="secondary" @click="openCreateDialog" class="q-mb-md" rounded />
 
     <q-table :rows="inventarios" :columns="columns" row-key="id" :loading="loading" v-model:pagination="pagination"
       :rows-per-page-label="'Registros por página:'" @request="onRequest" class="q-mb-md">
+      <template v-slot:top-left>
+        <!-- Botón para crear nuevo registro dentro de la tabla -->
+        <q-btn label="Crear" color="secondary" @click="openCreateDialog" class="q-mr-md" rounded />
+      </template>
       <template v-slot:top-right>
-        <q-input filled debounce="300" placeholder="Buscar..." v-model="search" @input="onSearch" clearable />
+        <!-- Campo de búsqueda -->
+        <q-input filled dense debounce="300" placeholder="Buscar..." v-model="search" @input="onSearch" clearable />
       </template>
 
       <!-- Slot para agregar botones de acción en cada fila -->
-      <template v-slot:body-cell-actions="props" class="q-virtual-scroll--with-prev">
+      <template v-slot:body-cell-actions="props">
         <q-td :props="props" class="q-pa-none">
           <q-btn flat color="secondary" icon="edit" @click="openEditDialog(props.row)" class="q-mr-xs" round />
           <q-btn flat color="negative" icon="delete" @click="confirmDelete(props.row.id)" round />
@@ -18,7 +21,7 @@
       </template>
     </q-table>
 
-    <!-- Diálogo para crear y editar registros -->
+    <!-- PopUp para crear y editar registros -->
     <q-dialog v-model="dialogVisible">
       <q-card class="q-pa-md" style="width: 800px;">
         <q-card-section>
@@ -28,7 +31,7 @@
             <q-col :cols="12" :md="6">
               <q-input v-model="currentItem.codigo" label="Código" outlined />
               <q-input v-model="currentItem.descripcion" label="Descripción" outlined />
-              <q-select v-model="currentItem.datos" label="Tipo" :options="datos"
+              <q-select v-model="currentItem.datos" label="Datos" :options="datos"
                 option-value="id" option-label="nombre" emit-value map-options outlined />
 
               <q-select v-model="currentItem.area_funcional_id" label="Área Funcional" :options="areasFuncionales"
@@ -40,7 +43,7 @@
               <q-select v-model="currentItem.sistema_id" label="Sistema" :options="sistemas" option-value="id"
                 option-label="sistema" emit-value map-options outlined />
 
-              <q-select v-model="currentItem.en_desarrollo" label="Estado" :options="enDesarrollo" option-value="id"
+              <q-select v-model="currentItem.estado" label="En Desarrollo" :options="enDesarrollo" option-value="id"
                 option-label="enDesarrollo" emit-value map-options outlined />
 
               <q-select v-model="currentItem.capa" label="Capa" :options="capa" option-value="id"
@@ -48,7 +51,7 @@
 
               <q-select v-model="currentItem.pais_id" :options="paises" label="Pais" option-label="nombre"
                 option-value="id" emit-value map-options outlined />
-              <!--q-input v-model="currentItem.en_desarrollo" label="En Desarrollo" outlined /-->
+              <!--q-input v-model="currentItem.estado" label="En Desarrollo" outlined /-->
             </q-col>
           </q-row>
         </q-card-section>
@@ -60,9 +63,7 @@
       </q-card>
     </q-dialog>
 
-
-
-    <!-- Diálogo de confirmación de eliminación -->
+    <!-- PopUp de confirmación de eliminación -->
     <q-dialog v-model="deleteDialogVisible">
       <q-card class="q-pa-md">
         <q-card-section>
@@ -70,7 +71,7 @@
         </q-card-section>
         <q-card-actions>
           <q-btn @click="deleteItem" color="negative" label="Eliminar" />
-          <q-btn @click="deleteDialogVisible = false" color="secondary" label="Cancelar" />
+          <q-btn @click="deleteDialogVisible = false" color="negative" label="Cancelar" />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -99,13 +100,13 @@ export default {
       itemToDelete: null,
       columns: [
         { name: 'codigo', label: 'Código', field: 'codigo', align: 'left', sortable: true },
-        { name: 'datos', label: 'Tipo', field: 'datos', align: 'left', sortable: true },
+        { name: 'datos', label: 'Datos', field: 'datos', align: 'left', sortable: true },
         { name: 'descripcion', label: 'Descripción', field: 'descripcion', align: 'left', sortable: true },
         { name: 'area_funcional', label: 'Área Funcional', field: row => row.areaFuncional?.nombre, align: 'left', sortable: true },
         { name: 'sistema', label: 'Sistema', field: row => row.sistema?.sistema, align: 'left', sortable: true },
         { name: 'pais', label: 'País', field: row => row.pais?.nombre, align: 'left', sortable: true },
         { name: 'usuario', label: 'Usuario', field: row => row.usuario?.username, align: 'left', sortable: true },
-        { name: 'en_desarrollo', label: 'Estado', field: 'en_desarrollo', align: 'center', sortable: true },
+        { name: 'estado', label: 'Estado', field: 'estado', align: 'center', sortable: true },
         { name: 'capa', label: 'Capa', field: 'capa', align: 'center', sortable: true },
         { name: 'actions', label: 'Acciones', align: 'center', field: 'actions' }
       ],
